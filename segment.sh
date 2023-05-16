501c6b876ae032b818d815237167cae991e2c080
Brad Caron <bacaron@utexas.edu>
2023-05-16 17:16:52 -0500

    reason: small fix

--- segment.sh
+++ segment.sh
@@ -8,7 +8,7 @@ mask=`jq -r '.mask' config.json`
 
 [ ! -f t1_mask.nii.gz ] && fslmaths ${anat} -mas ${mask} t1_mask.nii.gz
 
-fast -g -N -p t1_mask.nii.gz && cp -v *.nii.gz ./mask/
+fast -g -N -p t1_mask.nii.gz && mv *.nii.gz ./mask/
 
 if [ ! -f ./mask/t1_mask_seg.nii.gz ]; then
 	echo "something went wrong. check logs and derivatives"
@@ -17,3 +17,14 @@ fi
 
 echo "complete"
 mv ./mask/t1_mask_seg.nii.gz ./mask/mask.nii.gz
+mv ./mask/t1_mask_prob_0.nii.gz ./mask/CSF-probseg.nii.gz
+mv ./mask/t1_mask_prob_1.nii.gz ./mask/GM-probseg.nii.gz
+mv ./mask/t1_mask_prob_2.nii.gz ./mask/WM-probseg.nii.gz
+
+mv ./mask/t1_mask_seg_0.nii.gz ./mask/CSF-seg.nii.gz
+mv ./mask/t1_mask_seg_1.nii.gz ./mask/GM-seg.nii.gz
+mv ./mask/t1_mask_seg_2.nii.gz ./mask/WM-seg.nii.gz
+
+mv ./mask/t1_mask_pve_0.nii.gz ./mask/CSF-pveseg.nii.gz
+mv ./mask/t1_mask_pve_1.nii.gz ./mask/GM-pveseg.nii.gz
+mv ./mask/t1_mask_pve_2.nii.gz ./mask/WM-pveseg.nii.gz
