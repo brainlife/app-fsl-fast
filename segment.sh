d03d3c8bead0058b930f98050fe697a1f0fa758f
Brad Caron <bacaron@utexas.edu>
2023-05-16 16:59:50 -0500

    reason: initial commit

---
+++ segment.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# config inputs
+anat=`jq -r '.anat' config.json`
+mask=`jq -r '.mask' config.json`
+
+[ ! -d mask ] && mkdir mask
+
+[ ! -f t1_mask.nii.gz ] && fslmaths ${anat} -mas ${mask} t1_mask.nii.gz
+
+fast -g -N -p t1_mask.nii.gz && cp -v *.nii.gz ./mask/
+
+if [ ! -f ./mask/t1_mask_seg.niii.gz ]; then
+	echo "something went wrong. check logs and derivatives"
+	exit 1
+fi
+
+echo "complete"
+mv ./mask/t1_mask_seg.nii.gz ./mask/mask.nii.gz
